d77e84bce874c8728adea51962b06da37d9d4cfe
FrenzyVJN <svijayan01@gmail.com>
2023-10-08 23:04:49 +0530

    v5

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,18 @@ function page() {
                 <h2 className='text-white text-2xl font-bold'>Share your photos with friends</h2>
                 
     </div> */}
-            <div className="flex flex-col h-3/4 w-1/2 justify-center mt-auto mb-auto">
+            <div className="flex flex-col h-1/2 w-1/2 justify-center mt-auto mb-auto">
                 <form className="flex flex-col w-full bg-gradient-to-b from-cyan-800 to-blue-800 items-center rounded-xl h-3/4 justify-center gap-4 animate-movingBg">
-                    <h1 className="text-white font-bold text-center text-4xl">Sign Up</h1>
-                    <br></br>
+                    <h1 className="text-white font-bold text-center text-4xl">Post</h1>
                     <span className='flex w-1/2 items-center  justify-center bg-black bg-opacity-30 rounded-xl'>
                         <label className='w-fit h-full p-3 bg-black-500 rounded-l-xl '><BiSolidUserCircle className="h-full w-fit fill-white "/></label>
                         <input className="w-full text-white font-semibold text-2xl py-4 pl-2 bg-transparent  rounded-xl outline-none" placeholder="Username" />
                     </span>
-                    <span className='flex w-1/2  bg-black bg-opacity-30 rounded-xl'>
+                 {/*}   <span className='flex w-1/2  bg-black bg-opacity-30 rounded-xl'>
                         <label className='w-fit h-full p-3 bg-black-500 rounded-l-xl '><BiSolidKey className="h-full w-fit fill-white "/></label>
                         <input className="w-full text-white font-semibold text-2xl py-4 pl-2 bg-transparent  rounded-xl outline-none" placeholder="Password" />
-                    </span>
-                    <button className="w-1/3 text-xl bg-gradient-to-b from-cyan-600 to-blue-500 hover:bg-gradient-to-b hover:from-cyan-400 hover:to-blue-600 hover:scale-105 transition-transform  py-4 rounded-xl text-white font-semibold">Login</button>
+                    </span> */}
+                    <button className="w-1/3 text-xl bg-gradient-to-b from-cyan-600 to-blue-500 hover:bg-gradient-to-b hover:from-cyan-400 hover:to-blue-600 hover:scale-105 transition-transform  py-4 rounded-xl text-white font-semibold">Next</button>
                 </form>
             </div>
         </main>
